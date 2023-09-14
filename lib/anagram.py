97fa58d7e1affc6da81baa2a10da55d5a5cb327c
Vanessa Jesik <vajesik@gmail.com>
2023-09-14 10:55:08 -0600

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, possible_list):
+        return [possibility for possibility in possible_list if
+                sorted(possibility) == sorted(self.word)]
